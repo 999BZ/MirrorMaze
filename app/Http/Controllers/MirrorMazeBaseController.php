40455c9a84ceb6a04b9c3473eb2a2fcd6fa278de
Fisnik Hazrolli <hazrollifisnik@gmail.com>
2024-01-09 13:38:24 +0100

    Commit Changes

---
+++ app/Http/Controllers/MirrorMazeBaseController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MirrorMazeBaseController extends Controller
+{
+    //TODO: Implement the functions for the APIs
+}

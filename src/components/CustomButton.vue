336ddde316eb040162a6699c86cd767295475216
endritgjokaj <70038985+endritgjokaj@users.noreply.github.com>
2024-01-14 22:25:02 +0100

    2 UI elements added

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <button class="px-5 py-2 text-lg border-2 border-black bg-black/10" @click="this.$emit('do-action')">{{ text
+    }}</button>
+</template>
+
+<script>
+export default {
+    props: ['text'],
+    emits: ['do-action'],
+}
+</script>
